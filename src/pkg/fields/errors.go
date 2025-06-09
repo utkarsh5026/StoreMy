7242926f8fc844a269032017086c007d03b69ec9
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-06-10 00:30:35 +0530

    Remove all source files, including the go.mod and go.sum, as well as the .gitignore and field implementations, from the project.

--- src/pkg/fields/errors.go
+++
@@ -1,9 +0,0 @@
-package fields
-
-import "errors"
-
-var UnSupportedPredicate = errors.New("unsupported predicate")
-
-var TypeMismatch = errors.New("type mismatch")
-
-var InvalidDataType = errors.New("invalid data type")
