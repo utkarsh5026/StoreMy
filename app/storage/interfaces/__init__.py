289e2d8a791d80e6c06de99cd624f2feb67df136
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-06-14 00:59:14 +0530

    Add page handling and validation exceptions
    - Introduced `IOException` and various `PageValidationError` subclasses to handle specific validation issues related to page types, sizes, headers, and slots.
    - Implemented `HeapPageHandler` class for managing heap pages, including methods for loading, writing, and validating pages.
    - Enhanced modularity by defining an abstract `PageHandler` interface for consistent page management across different storage implementations.
    - Updated `__init__.py` to include the new `PageHandler` interface in the module exports.

--- app/storage/interfaces/__init__.py
+++ app/storage/interfaces/__init__.py
@@ -8,5 +8,6 @@ which are used to abstract the underlying storage implementation.
 from .db_file import DbFile
 from .page_id import PageId
 from .page import Page
+from .page_handler import PageHandler
 
-__all__ = ['DbFile', 'PageId', 'Page']
+__all__ = ['DbFile', 'PageId', 'Page', 'PageHandler']
