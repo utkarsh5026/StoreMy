794fbe80155676b48d4505630dd4bd6bd30a6fe0
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-18 04:02:00 +0530

    Enhance Field interface documentation with detailed method descriptions

--- pkg/types/field.go
+++ pkg/types/field.go
@@ -2,16 +2,26 @@ package types
 
 import "io"
 
+// Field represents a generic field that can be serialized, compared, and hashed.
+// It provides methods for type checking, string representation, equality, and comparison operations.
 type Field interface {
+	// Serialize writes the field's data to the provided writer.
 	Serialize(w io.Writer) error
 
+	// Compare evaluates the field against another field using the given predicate operation.
+	// Returns true if the comparison holds, or an error if the operation is invalid.
 	Compare(op Predicate, other Field) (bool, error)
 
+	// Type returns the type of the field.
 	Type() Type
 
+	// String returns a string representation of the field.
 	String() string
 
+	// Equals checks if the field is equal to another field.
 	Equals(other Field) bool
 
+	// Hash computes a hash value for the field.
+	// Returns the hash as a uint32, or an error if hashing fails.
 	Hash() (uint32, error)
 }
