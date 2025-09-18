b3d6bcead9aee8b05bc7cb80d1330c49e70d0e42
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-18 21:43:00 +0530

    Add HeapPageID struct and associated methods for heap page identification and management

---
+++ pkg/storage/heap/pageid.go
@@ -0,0 +1,55 @@
+package heap
+
+import (
+	"fmt"
+	"storemy/pkg/tuple"
+)
+
+// HeapPageID represents a unique identifier for a heap page
+type HeapPageID struct {
+	tableID int // The table this page belongs to
+	pageNum int // The page number within the table
+}
+
+// NewHeapPageID creates a new heap page ID
+func NewHeapPageID(tableID, pageNum int) *HeapPageID {
+	return &HeapPageID{
+		tableID: tableID,
+		pageNum: pageNum,
+	}
+}
+
+// GetTableID returns the table ID
+func (hpid *HeapPageID) GetTableID() int {
+	return hpid.tableID
+}
+
+// PageNo returns the page number
+func (hpid *HeapPageID) PageNo() int {
+	return hpid.pageNum
+}
+
+// Serialize returns this page ID as an array of integers
+func (hpid *HeapPageID) Serialize() []int {
+	return []int{hpid.tableID, hpid.pageNum}
+}
+
+// Equals checks if two heap page IDs are equal
+func (hpid *HeapPageID) Equals(other tuple.PageID) bool {
+	otherHeap, ok := other.(*HeapPageID)
+	if !ok {
+		return false
+	}
+	return hpid.tableID == otherHeap.tableID && hpid.pageNum == otherHeap.pageNum
+}
+
+// String returns a string representation of this heap page ID
+func (hpid *HeapPageID) String() string {
+	return fmt.Sprintf("HeapPageID(table=%d, page=%d)", hpid.tableID, hpid.pageNum)
+}
+
+// HashCode returns a hash code for this heap page ID
+func (hpid *HeapPageID) HashCode() int {
+	// Combine table ID and page number into a single hash
+	return hpid.tableID*1000 + hpid.pageNum
+}
