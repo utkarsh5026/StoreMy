f51cb6f821a01f85a98b6e94174828834ea4e595
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-19 03:21:07 +0530

    Implement TableInfo struct and associated methods for table metadata management

---
+++ pkg/tables/table.go
@@ -0,0 +1,36 @@
+package tables
+
+import (
+	"fmt"
+	"storemy/pkg/storage"
+	"storemy/pkg/tuple"
+)
+
+// TableInfo holds metadata about a table
+type TableInfo struct {
+	File       storage.DbFile          // The file storing the table data
+	Name       string                  // The table name
+	PrimaryKey string                  // Primary key field name
+	TupleDesc  *tuple.TupleDescription // Schema of the table
+}
+
+// NewTableInfo creates a new table info instance
+func NewTableInfo(file storage.DbFile, name, primaryKey string) *TableInfo {
+	return &TableInfo{
+		File:       file,
+		Name:       name,
+		PrimaryKey: primaryKey,
+		TupleDesc:  file.GetTupleDesc(),
+	}
+}
+
+// GetID returns the table's unique identifier
+func (ti *TableInfo) GetID() int {
+	return ti.File.GetID()
+}
+
+// String returns a string representation of the table info
+func (ti *TableInfo) String() string {
+	return fmt.Sprintf("Table(%s, id=%d, schema=%s, pk=%s)",
+		ti.Name, ti.GetID(), ti.TupleDesc.String(), ti.PrimaryKey)
+}
