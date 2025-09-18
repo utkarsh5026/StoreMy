4fe09227255f899118ffd0ce120a71f83368db5a
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-19 05:14:40 +0530

    Define DbIterator interface for database tuple iteration methods

--- pkg/execution/iterator.go
+++ pkg/execution/iterator.go
@@ -5,6 +5,15 @@ import (
 	"storemy/pkg/tuple"
 )
 
+type DbIterator interface {
+	Open() error
+	HasNext() (bool, error)
+	Next() (*tuple.Tuple, error)
+	Rewind() error
+	Close() error
+	GetTupleDesc() *tuple.TupleDescription
+}
+
 // ReadNextFunc is the function signature for reading the next tuple from an iterator.
 // Returns:
 //   - *tuple.Tuple: Next tuple from the data source, or nil if no more tuples
