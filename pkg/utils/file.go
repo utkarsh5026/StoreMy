d54b26177df5a2bf83b9b8bbe2eb702534189173
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-19 04:15:30 +0530

    Add OpenFile function to handle file opening with error handling

---
+++ pkg/utils/file.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+// openFile opens a file with appropriate flags
+func OpenFile(filename string) (*os.File, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %s: %v", filename, err)
+	}
+	return file, nil
+}
