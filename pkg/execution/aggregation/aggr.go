57c5dcfbd6506c6646140148d9871fa652cc1033
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-20 17:26:37 +0530

    Add Aggregation implementation with AggregateOp types and Aggregator interface. This includes methods for merging tuples, iterating over results, and retrieving tuple descriptions, establishing a foundation for aggregation operations in the execution package.

---
+++ pkg/execution/aggregation/aggr.go
@@ -0,0 +1,55 @@
+package aggregation
+
+import (
+	"storemy/pkg/iterator"
+	"storemy/pkg/tuple"
+)
+
+const (
+	// NoGrouping indicates that no grouping field is used in aggregation
+	NoGrouping = -1
+)
+
+// AggregateOp represents the type of aggregation operation to perform
+type AggregateOp int
+
+const (
+	Min AggregateOp = iota
+	Max
+	Sum
+	Avg
+	Count
+)
+
+// String returns a string representation of the aggregation operation
+func (op AggregateOp) String() string {
+	switch op {
+	case Min:
+		return "MIN"
+	case Max:
+		return "MAX"
+	case Sum:
+		return "SUM"
+	case Avg:
+		return "AVG"
+	case Count:
+		return "COUNT"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+// Aggregator interface defines the contract for aggregation operations
+// This is the fundamental interface that all aggregators must implement
+type Aggregator interface {
+	// Merge processes a new tuple into the aggregate, grouping as specified
+	// This is where the actual aggregation computation happens
+	Merge(tup *tuple.Tuple) error
+
+	// Iterator returns a DbIterator over the aggregate results
+	// Results are tuples containing either (aggregateValue) or (groupValue, aggregateValue)
+	Iterator() iterator.DbIterator
+
+	// GetTupleDesc returns the tuple description for the aggregate results
+	GetTupleDesc() *tuple.TupleDescription
+}
