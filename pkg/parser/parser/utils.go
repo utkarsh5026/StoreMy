7c844905efab2436316e27e8a99c3d7d96d0a509
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-23 20:16:21 +0530

    Add parseValue function for value parsing in SQL statements
    This commit introduces the parseValue function, which handles the parsing of different value types (STRING, INT, NULL) in the SQL parser. It enhances the parser's ability to process field values by converting tokens into appropriate types and includes error handling for invalid inputs. This addition improves the overall functionality and robustness of the SQL parser.

--- pkg/parser/parser/utils.go
+++ pkg/parser/parser/utils.go
@@ -3,6 +3,8 @@ package parser
 import (
 	"fmt"
 	"storemy/pkg/parser/lexer"
+	"storemy/pkg/types"
+	"strconv"
 )
 
 func expectToken(t lexer.Token, expected lexer.TokenType) error {
@@ -10,4 +12,23 @@ func expectToken(t lexer.Token, expected lexer.TokenType) error {
 		return fmt.Errorf("expected %s, got %s", expected.String(), t.Value)
 	}
 	return nil
+}
+
+func parseValue(l *lexer.Lexer) (types.Field, error) {
+	token := l.NextToken()
+
+	switch token.Type {
+	case lexer.STRING:
+		return types.NewStringField(token.Value, types.StringMaxSize), nil
+	case lexer.INT:
+		value, err := strconv.Atoi(token.Value)
+		if err != nil {
+			return nil, fmt.Errorf("invalid integer value: %s", token.Value)
+		}
+		return types.NewIntField(int32(value)), nil
+	case lexer.NULL:
+		return nil, nil // NULL value
+	default:
+		return nil, fmt.Errorf("expected value, got %s", token.Value)
+	}
 }
