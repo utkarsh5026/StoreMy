452abe6d5f7727a2db475f98c40a68b6c1c0b1ce
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-24 19:40:27 +0530

    Add parseUpdateStatement function for SQL UPDATE statement parsing
    This commit introduces the parseUpdateStatement function, which processes SQL UPDATE statements by validating the syntax and extracting relevant components such as table names, field names, and values. It also supports optional WHERE clauses for filtering updates. This addition enhances the SQL parser's functionality, enabling it to handle UPDATE operations effectively and improving overall SQL statement parsing capabilities.

---
+++ pkg/parser/parser/update.go
@@ -0,0 +1,73 @@
+package parser
+
+import (
+	"fmt"
+	"storemy/pkg/parser/lexer"
+	"storemy/pkg/parser/statements"
+)
+
+func parseUpdateStatement(l *lexer.Lexer) (*statements.UpdateStatement, error) {
+	token := l.NextToken()
+	if err := expectToken(token, lexer.UPDATE); err != nil {
+		return nil, err
+	}
+
+	token = l.NextToken()
+	if err := expectToken(token, lexer.IDENTIFIER); err != nil {
+		return nil, err
+	}
+
+	tableName := token.Value
+	alias := tableName
+
+	token = l.NextToken()
+	if token.Type == lexer.IDENTIFIER {
+		alias = token.Value
+		token = l.NextToken()
+	}
+
+	if err := expectToken(token, lexer.SET); err != nil {
+		return nil, err
+	}
+
+	stmt := statements.NewUpdateStatement(tableName, alias)
+	for {
+		token = l.NextToken()
+		if err := expectToken(token, lexer.IDENTIFIER); err != nil {
+			return nil, err
+		}
+		fieldName := token.Value
+
+		token = l.NextToken()
+		if token.Type != lexer.OPERATOR || token.Value != "=" {
+			return nil, fmt.Errorf("expected '=', got %s", token.Value)
+		}
+
+		value, err := parseValue(l)
+		if err != nil {
+			return nil, err
+		}
+
+		stmt.AddSetClause(fieldName, value)
+		token = l.NextToken()
+		if token.Type == lexer.COMMA {
+			continue
+		} else {
+			l.SetPos(token.Position) // Put it back
+			break
+		}
+	}
+
+	token = l.NextToken()
+	if token.Type == lexer.WHERE {
+		filter, err := parseWhereCondition(l)
+		if err != nil {
+			return nil, err
+		}
+		stmt.SetWhereClause(filter)
+	} else {
+		l.SetPos(token.Position)
+	}
+
+	return stmt, nil
+}
