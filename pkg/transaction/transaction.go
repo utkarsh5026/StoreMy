b793a236ec94e595e7a4f357c3ec271f603829bb
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-09-25 05:52:25 +0530

    Add TransactionID type for unique transaction identification
    This commit introduces a new TransactionID type that generates unique transaction identifiers using an atomic counter. The TransactionID struct includes methods for retrieving the ID, converting it to a string format, and comparing two TransactionID instances for equality. This addition lays the foundation for managing transactions effectively within the application.

---
+++ pkg/transaction/transaction.go
@@ -0,0 +1,33 @@
+package transaction
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+var transactionCounter int64
+
+type TransactionID struct {
+	id int64
+}
+
+func NewTransactionID() *TransactionID {
+	return &TransactionID{
+		id: atomic.AddInt64(&transactionCounter, 1),
+	}
+}
+
+func (tid *TransactionID) ID() int64 {
+	return tid.id
+}
+
+func (tid *TransactionID) String() string {
+	return fmt.Sprintf("TID-%d", tid.id)
+}
+
+func (tid *TransactionID) Equals(other *TransactionID) bool {
+	if tid == nil || other == nil {
+		return tid == other
+	}
+	return tid.id == other.id
+}
