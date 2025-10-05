c77c7182b4b4cbba574623b386b2dcb6a0688fcb
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-10-05 11:23:37 +0530

    Enhance QueryPlanner to support data directory and new statement types
    This commit updates the QueryPlanner struct to include a dataDir field and introduces a SetDataDir method for setting the data directory. Additionally, it modifies the Plan method to accommodate new statement types, specifically SelectStatement and UpdateStatement, ensuring that the corresponding plans are created with the necessary parameters. These changes improve the flexibility and functionality of the query planning process.

--- pkg/planner/query.go
+++ pkg/planner/query.go
@@ -14,23 +14,33 @@ type Plan interface {
 type QueryPlanner struct {
 	tableManager *memory.TableManager
 	pageStore    *memory.PageStore
+	dataDir      string
 }
 
 func NewQueryPlanner(t *memory.TableManager, ps *memory.PageStore) *QueryPlanner {
 	return &QueryPlanner{
 		tableManager: t,
 		pageStore:    ps,
+		dataDir:      "",
 	}
+}
+
+func (qp *QueryPlanner) SetDataDir(dataDir string) {
+	qp.dataDir = dataDir
 }
 
 func (qp *QueryPlanner) Plan(stmt statements.Statement, tid *transaction.TransactionID) (Plan, error) {
 	switch s := stmt.(type) {
 	case *statements.CreateStatement:
-		return NewCreateTablePlan(s, qp.tableManager, tid), nil
+		return NewCreateTablePlan(s, qp.tableManager, tid, qp.dataDir), nil
 	case *statements.InsertStatement:
 		return NewInsertPlan(s, qp.pageStore, tid, qp.tableManager), nil
 	case *statements.DeleteStatement:
 		return NewDeletePlan(s, qp.pageStore, tid, qp.tableManager), nil
+	case *statements.SelectStatement:
+		return NewSelectPlan(s, qp.tableManager, qp.pageStore, tid), nil
+	case *statements.UpdateStatement:
+		return NewUpdatePlan(s, qp.tableManager, qp.pageStore, tid), nil
 	default:
 		return nil, fmt.Errorf("unsupported statement type: %T", stmt)
 	}
