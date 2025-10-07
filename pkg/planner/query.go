e49fccdc5e063f254d553acb489b4a4722904d96
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-10-07 14:06:23 +0530

    Refactor QueryPlanner to utilize transaction.TransactionContext for improved transaction handling
    This commit updates the Plan method in query.go to replace the TransactionID with TransactionContext, enhancing the management of transaction contexts during query planning. This change ensures consistency and improved type safety across the codebase, aligning with recent updates in the transaction management system.

--- pkg/planner/query.go
+++ pkg/planner/query.go
@@ -2,8 +2,8 @@ package planner
 
 import (
 	"fmt"
+	"storemy/pkg/concurrency/transaction"
 	"storemy/pkg/parser/statements"
-	"storemy/pkg/primitives"
 	"storemy/pkg/registry"
 )
 
@@ -21,18 +21,18 @@ func NewQueryPlanner(ctx *registry.DatabaseContext) *QueryPlanner {
 	}
 }
 
-func (qp *QueryPlanner) Plan(stmt statements.Statement, tid *primitives.TransactionID) (Plan, error) {
+func (qp *QueryPlanner) Plan(stmt statements.Statement, tx *transaction.TransactionContext) (Plan, error) {
 	switch s := stmt.(type) {
 	case *statements.CreateStatement:
-		return NewCreateTablePlan(s, qp.ctx, tid), nil
+		return NewCreateTablePlan(s, qp.ctx, tx), nil
 	case *statements.InsertStatement:
-		return NewInsertPlan(s, tid, qp.ctx), nil
+		return NewInsertPlan(s, tx, qp.ctx), nil
 	case *statements.DeleteStatement:
-		return NewDeletePlan(s, tid, qp.ctx), nil
+		return NewDeletePlan(s, tx, qp.ctx), nil
 	case *statements.SelectStatement:
-		return NewSelectPlan(s, tid, qp.ctx), nil
+		return NewSelectPlan(s, tx, qp.ctx), nil
 	case *statements.UpdateStatement:
-		return NewUpdatePlan(s, tid, qp.ctx), nil
+		return NewUpdatePlan(s, tx, qp.ctx), nil
 	default:
 		return nil, fmt.Errorf("unsupported statement type: %T", stmt)
 	}
