f179ddf137346b01c89c8261baae65658598b8d4
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-10-15 20:57:05 +0530

    Refactor HashString function to use FNV-1a algorithm for improved hash distribution and performance.

--- pkg/utils/hash.go
+++ pkg/utils/hash.go
@@ -1,10 +1,14 @@
 package utils
 
-// HashString creates a simple hash of a string that fits in int32 range
+import (
+	"hash/fnv"
+)
+
+// HashString creates a hash of a string using FNV-1a algorithm.
+// FNV-1a provides good distribution and is fast for small inputs like file paths.
+// Returns a 32-bit hash value cast to int for compatibility.
 func HashString(s string) int {
-	hash := int32(0)
-	for _, c := range s {
-		hash = 31*hash + int32(c)
-	}
-	return int(hash)
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return int(h.Sum32())
 }
